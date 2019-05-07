e6b0c85318a7571dbd6ff79f865039022832d1e4
Mason France <masonrf1@gmail.com>
2019-05-07 13:01:41 -0600

    Write logic for seeding

--- db/seeds/dev/sampleData.js
+++ db/seeds/dev/sampleData.js
@@ -1,13 +1,50 @@
+const sampleData = require('../../sampleData')
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+const createProject = (knex, project) => {
+  return knex("projects")
+    .insert(
+      {
+        name: project.name,
+      },
+      'id'
+    )
+    .then(projectID => {
+      let palettePromises = [];
+
+      project.palettes.forEach(palette => {
+        palettePromises.push(
+          createPalette(knex, {
+            name: palette.name,
+            color1: palette.color1,
+            color2: palette.color2,
+            color3: palette.color3,
+            color4: palette.color4,
+            color5: palette.color5,
+            projectID: projectID[0]
+          })
+        );
+      });
+
+      return Promise.all(palettePromises);
     });
+};
+
+const createPalette = (knex, palette) => {
+  return knex("palettes").insert(palette);
+};
+
+exports.seed = (knex, Promise) => {
+  return knex("palettes")
+    .del()
+    .then(() => knex("projects").del())
+    .then(() => {
+      let projectPromises = [];
+
+      data.forEach(project => {
+        projectPromises.push(createProject(knex, project));
+      });
+
+      return Promise.all(projectPromises);
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
 };
