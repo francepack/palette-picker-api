9b12cf834ad05d3e240d6d7072e3e01e82c1140d
Mason France <masonrf1@gmail.com>
2019-05-07 12:55:02 -0600

    Add initial migration, Initialize seed file

---
+++ db/sampleData.js
@@ -0,0 +1,67 @@
+const sampleData = [
+  {
+    name: 'project1',
+    palettes: [
+      {
+        name: 'palette1',
+        color1: 'blue',
+        color2: 'red',
+        color3: 'yellow',
+        color4: 'green',
+        color5: 'purple',
+      },
+      {
+        name: 'palette2',
+        color1: 'redder',
+        color2: 'yellowish',
+        color3: 'blarg',
+        color4: 'vomit',
+        color5: 'purps',
+      }
+    ]
+  },
+  {
+    name: 'project2',
+    palettes: [
+      {
+        name: 'palette3',
+        color1: 'yes',
+        color2: 'no',
+        color3: 'maybe',
+        color4: 'so',
+        color5: 'yesagain',
+      },
+      {
+        name: 'palette4',
+        color1: 'a',
+        color2: 'b',
+        color3: 'c',
+        color4: 'd',
+        color5: 'e',
+      }
+    ]
+  },
+  {
+    name: 'project3',
+    palettes: [
+      {
+        name: 'palette5',
+        color1: '123',
+        color2: '234',
+        color3: '435',
+        color4: '567',
+        color5: '865',
+      },
+      {
+        name: 'palette6',
+        color1: 'value',
+        color2: 'sample',
+        color3: 'example',
+        color4: 'mock',
+        color5: 'jest',
+      }
+    ]
+  }
+]
+
+module.exports = sampleData;
--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,12 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/palette-picker',
+    connection: 'postgres://localhost/picker',
     migrations: {
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   },
